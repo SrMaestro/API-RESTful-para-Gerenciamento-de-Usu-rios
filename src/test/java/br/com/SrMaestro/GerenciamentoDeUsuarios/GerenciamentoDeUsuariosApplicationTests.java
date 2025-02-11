03b221fc5c177348d582afed9f2ff59d5ad407a8
Luis Felipe R . L <luisramires29032002@gmail.com>
2025-02-11 11:23:43 -0300

    chore: config ambiente

---
+++ src/test/java/br/com/SrMaestro/GerenciamentoDeUsuarios/GerenciamentoDeUsuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.SrMaestro.GerenciamentoDeUsuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeUsuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
